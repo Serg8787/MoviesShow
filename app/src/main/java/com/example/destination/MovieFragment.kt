97971cbac0a5b4d370a5e94a6e49e36c6dc88816
Serg8787 <tsybulnikserg@gmail.com>
2021-09-04 18:32:34 +0300

    add load popularity and topRated mainViewmodel

--- app/src/main/java/com/example/destination/MovieFragment.kt
+++ app/src/main/java/com/example/destination/MovieFragment.kt
@@ -11,14 +11,9 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.destination.adapter.MovieAdapter
-import com.example.destination.model.MovieResponse
 import com.example.destination.model.MovieResult
-import com.example.destination.network.API
-import com.example.destination.network.RetrofitClient
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_movie.*
-import retrofit2.Call
-import retrofit2.Response
 
 
 /**
@@ -28,15 +23,13 @@ import retrofit2.Response
  */
 class MovieFragment : Fragment() {
     var adapter: MovieAdapter? = null
-    var list: ArrayList<MovieResult>?=null
+    var list: ArrayList<MovieResult>? = null
     private lateinit var viewModel: MovieViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         (activity as AppCompatActivity?)!!.bottomNavView.visibility = View.VISIBLE
         viewModel = ViewModelProvider(this).get(MovieViewModel::class.java)
-
-
 
     }
 
@@ -51,17 +44,17 @@ class MovieFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-            getPopularityMoviesData()
-//            getTopRatedMoviesData()
+        getPopularityMoviesData()
+            getTopRatedMoviesData()
 //            getLatestMoviesData()
 
     }
 
     fun getPopularityMoviesData() {
         val adapter = MovieAdapter()
-
         rvPopularityMovie.adapter = adapter
-        viewModel.listMovie.observe(viewLifecycleOwner, Observer {
+
+        viewModel.listMoviePopularity.observe(viewLifecycleOwner, Observer {
             Log.i("MyLog", "" + it)
             rvPopularityMovie.layoutManager = LinearLayoutManager(
                 MainActivity(),
@@ -71,52 +64,24 @@ class MovieFragment : Fragment() {
             adapter.movieList = it as ArrayList<MovieResult>
         })
 
-
-
-
-
-//        val retrofit = RetrofitClient.getClient("https://api.themoviedb.org/3/").create(API::class.java)
-//        retrofit.getPopularityMovies("fa98e12ff4452abc0e83ab5585e62d3c")
-//            .enqueue(object : retrofit2.Callback<MovieResponse> {
-//                override fun onResponse(
-//                    call: Call<MovieResponse>, response: Response<MovieResponse> ) {
-//                        list = response.body()!!.results as ArrayList<MovieResult>
-//                        adapter = MovieAdapter(context!!.applicationContext, list)
-//                        rvPopularityMovie.layoutManager = LinearLayoutManager(context!!.applicationContext,LinearLayoutManager.HORIZONTAL, false);
-//                        rvPopularityMovie.setAdapter(adapter);
-//
-//
-//                }
-//                override fun onFailure(call: Call<MovieResponse>, t: Throwable) {
-//                    Log.d("MyLog","bad"+t.toString())
-//                }
-//            })
     }
 
-//    fun getTopRatedMoviesData() {
-//        val retrofit =
-//            RetrofitClient.getClient("https://api.themoviedb.org/3/").create(API::class.java)
-//        retrofit.getTopRatedMovies("fa98e12ff4452abc0e83ab5585e62d3c")
-//            .enqueue(object : retrofit2.Callback<MovieResponse> {
-//                override fun onResponse(
-//                    call: Call<MovieResponse>,
-//                    response: Response<MovieResponse>
-//                ) {
-//                    list = response.body()!!.results as ArrayList<MovieResult>
-//                    adapter = MovieAdapter(context!!.applicationContext, list)
-//                    rvTopRatedMovie.layoutManager = LinearLayoutManager(
-//                        context!!.applicationContext,
-//                        LinearLayoutManager.HORIZONTAL,
-//                        false
-//                    );
-//                    rvTopRatedMovie.setAdapter(adapter);
-//                }
-//
-//                override fun onFailure(call: Call<MovieResponse>, t: Throwable) {
-//                    Log.d("MyLog", "bad" + t.toString())
-//                }
-//            })
-//    }
+    fun getTopRatedMoviesData() {
+        val adapter = MovieAdapter()
+
+        rvTopRatedMovie.adapter = adapter
+        viewModel.listMovieTopRated.observe(viewLifecycleOwner, Observer {
+            Log.i("MyLog", "" + it)
+            rvTopRatedMovie.layoutManager = LinearLayoutManager(
+                MainActivity(),
+                LinearLayoutManager.HORIZONTAL,
+                false
+            )
+            adapter.movieList = it as ArrayList<MovieResult>
+        })
+    }
+}
+
 //
 //    fun getLatestMoviesData() {
 //        val retrofit =
@@ -141,4 +106,4 @@ class MovieFragment : Fragment() {
 //                    Log.d("MyLog", "bad" + t.toString())
 //                }
 //            })
-    }
+
--- app/src/main/java/com/example/destination/MovieViewModel.kt
+++ app/src/main/java/com/example/destination/MovieViewModel.kt
@@ -1,41 +1,54 @@
 package com.example.destination
 
-import android.app.Application
 import android.util.Log
-import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.destination.adapter.MovieAdapter
 import com.example.destination.model.MovieResponse
 import com.example.destination.model.MovieResult
 import com.example.destination.network.API
 import com.example.destination.network.RetrofitClient
-import kotlinx.android.synthetic.main.fragment_movie.*
 import retrofit2.Call
 import retrofit2.Response
 
 class MovieViewModel() : ViewModel() {
 
-    val listMovie = MutableLiveData<List<MovieResult>>()
+    val listMoviePopularity = MutableLiveData<List<MovieResult>>()
+    val listMovieTopRated = MutableLiveData<List<MovieResult>>()
     init {
-        loadPopulaty()
+        loadPopulatyMovies()
+        loadTopRatedMovies()
     }
 
 
 
-    fun loadPopulaty(){
+    fun loadPopulatyMovies(){
         val retrofit = RetrofitClient.getClient("https://api.themoviedb.org/3/").create(API::class.java)
         retrofit.getPopularityMovies("fa98e12ff4452abc0e83ab5585e62d3c")
             .enqueue(object : retrofit2.Callback<MovieResponse> {
                 override fun onResponse(
                     call: Call<MovieResponse>, response: Response<MovieResponse>
                 ) {
-                    listMovie.postValue(response.body()!!.results as ArrayList<MovieResult>)
+                    listMoviePopularity.postValue(response.body()!!.results as ArrayList<MovieResult>)
 
                 }
                 override fun onFailure(call: Call<MovieResponse>, t: Throwable) {
-                    Log.d("MyLog","bad"+t.toString())
+                    Log.d("MyLog","Mistake"+t.message)
+                }
+            })
+    }
+    fun loadTopRatedMovies(){
+        val retrofit = RetrofitClient.getClient("https://api.themoviedb.org/3/").create(API::class.java)
+        retrofit.getTopRatedMovies("fa98e12ff4452abc0e83ab5585e62d3c")
+            .enqueue(object : retrofit2.Callback<MovieResponse>{
+                override fun onResponse(
+                    call: Call<MovieResponse>,
+                    response: Response<MovieResponse>
+                ) {
+                    listMovieTopRated.postValue(response.body()!!.results as ArrayList<MovieResult>)
+                }
+
+                override fun onFailure(call: Call<MovieResponse>, t: Throwable) {
+                    Log.d("MyLog","Mistake"+t.message)
                 }
             })
     }
